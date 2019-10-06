c3053aa26ef0dd830c8393dfc0a73076b26b6430
helloye <hello.paingy@gmail.com>
2019-10-06 10:32:38 -0400

    fixed reading data for python 3, adding pymongo db class

--- project1/src/load_data.py
+++ project1/src/load_data.py
@@ -1,13 +1,15 @@
 import csv
 
 # Loading nodes...
-with open('../data/nodes.tsv','rb') as tsvin:
-    tsvin = csv.reader(tsvin, delimiter='\t')
-    for row in tsvin:
+with open('../data/nodes.tsv') as tsvin:
+    reader = csv.reader(tsvin, delimiter='\t')
+    for row in reader:
         print(row)
 
 # Loading edges...
-with open('../data/edges.tsv','rb') as tsvin:
-    tsvin = csv.reader(tsvin, delimiter='\t')
-    for row in tsvin:
+with open('../data/edges.tsv') as tsvin:
+    reader = csv.reader(tsvin, delimiter='\t')
+    for row in reader:
         print(row)
+
+tsvin.close()
