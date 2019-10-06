5533706978ec573fdbbe3356157594ed7a3b4053
helloye <hello.paingy@gmail.com>
2019-10-06 00:48:30 -0400

    added initial file read of tsv into array

--- project1/src/load_data.py
+++ project1/src/load_data.py
@@ -1,1 +1,13 @@
-print('Hello World')
+import csv
+
+# Loading nodes...
+with open('../data/nodes.tsv','rb') as tsvin:
+    tsvin = csv.reader(tsvin, delimiter='\t')
+    for row in tsvin:
+        print(row)
+
+# Loading edges...
+with open('../data/edges.tsv','rb') as tsvin:
+    tsvin = csv.reader(tsvin, delimiter='\t')
+    for row in tsvin:
+        print(row)
