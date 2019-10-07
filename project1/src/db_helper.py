95d37ddab567ecf2a7dceb58ea840ecd5ff87988
helloye <hello.paingy@gmail.com>
2019-10-06 20:20:00 -0400

    add db connection tester and started initial parsing algorithm

--- project1/src/db_helper.py
+++ project1/src/db_helper.py
@@ -1,5 +1,14 @@
 from pymongo import MongoClient
+from pymongo.errors import ServerSelectionTimeoutError
 
 
 def get_client():
     return MongoClient('localhost', 27017)
+
+
+def test_connection(client):
+    try:
+        info = client.server_info() # Forces a call.
+        return info
+    except ServerSelectionTimeoutError:
+        return "Server Error"
--- project1/src/load_data.py
+++ project1/src/load_data.py
@@ -2,13 +2,14 @@ import csv
 from db_helper import get_client
 
 dbClient = get_client()
-print(dbClient)
 
 # Loading nodes...
 with open('../data/nodes.tsv') as tsvin:
     reader = csv.reader(tsvin, delimiter='\t')
     for row in reader:
-        print(row)
+        id_data = row[0].split('::')
+        print(id_data)
+        print(row[0] + " - " + row[1] + " - " + row[2] + "\n")
 
 # Loading edges...
 with open('../data/edges.tsv') as tsvin:
