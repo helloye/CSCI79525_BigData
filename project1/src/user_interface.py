8f462cba826013fabbce98ef9e498784f99e8709
helloye <hello.paingy@gmail.com>
2019-10-08 19:08:59 -0400

    adding a query interface

--- project1/src/user_interface.py
+++ project1/src/user_interface.py
@@ -1,6 +1,7 @@
 """
 This file will serve as the main user interface with the db
 """
+import os
 from db_helper import get_client, query_data
 
 DATABASE = 'csci79525_proj1'
@@ -8,7 +9,21 @@ DATABASE = 'csci79525_proj1'
 db_client = get_client()
 db = db_client[DATABASE]
 
-res = query_data({}, db.genes, 10)
+os.system('cls' if os.name=='nt' else 'clear')
+user_input = input("Identifier:")
+while user_input != "quit":
 
-for doc in res:
-    print(doc)
+    collection_to_query = input("Collection to query:")
+
+    res = query_data({"identifier": {'$regex': user_input}}, db[collection_to_query], 10)
+
+    print("\n Results: \n")
+    for doc in res:
+        print(doc)
+
+    input("\nPress enter to continue...")
+
+    os.system('cls' if os.name=='nt' else 'clear')
+    user_input = input("Identifier:")
+
+os.system('cls' if os.name=='nt' else 'clear')
