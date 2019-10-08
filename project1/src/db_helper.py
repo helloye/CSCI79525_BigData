2d88eb438a99fea16ad8d5ce1e4581c16cbcb3c0
helloye <hello.paingy@gmail.com>
2019-10-08 00:09:17 -0400

    added insert progress printer and db constants

--- project1/src/db_helper.py
+++ project1/src/db_helper.py
@@ -1,9 +1,10 @@
+import os
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 
 
 def get_client():
-    return MongoClient('localhost', 27017)
+    return MongoClient('localhost:27017')
 
 
 def test_connection(client):
@@ -13,8 +14,28 @@ def test_connection(client):
     except ServerSelectionTimeoutError:
         return "Server Error"
 
+
 def insert_data(data, col):
     try:
         col.insert_one(data)
     except:
         print('Insert Error')
+
+
+def drop_db(db_client, db):
+    try:
+        db_client.drop_database(db)
+    except:
+        print('Drop DB Error')
+
+
+# *NOTE: This is a very expensive operation. Set debug flag to False to make program run faster
+def insert_progress_printer(total_lines, current_count, data_type='', print_every_n_count=1, debug=True):
+    if debug and current_count % print_every_n_count == 0:
+        os.system('cls' if os.name=='nt' else 'clear')
+        percentage = round((current_count/total_lines) * 100 , 2)
+        print(data_type + ' Progress: ' + str(percentage) + '%')
+    # If debug flag is false, just print after every 100000 lines
+    elif not debug and current_count % 100000 == 0:
+        percentage = round((current_count/total_lines) * 100 , 2)
+        print(data_type + ' Progress: ' + str(percentage) + '%')
--- project1/src/load_data.py
+++ project1/src/load_data.py
@@ -1,31 +1,50 @@
 import csv
-from db_helper import get_client, insert_data
+from db_helper import get_client, insert_data, drop_db, insert_progress_printer
+
+# Actual
+# DATABASE = 'csci79525_proj1'
+
+# Test
+DATABASE = 'test'
+
 
 db_client = get_client()
-db = db_client.test
+# Clean db before loading new data
+drop_db(db_client, DATABASE);
+
+db = db_client[DATABASE]
 
 # Loading nodes...
+node_file_total_lines = sum(1 for line in open('../data/nodes.tsv'))
 with open('../data/nodes.tsv') as tsvin:
     reader = csv.reader(tsvin, delimiter='\t')
+    count = 0
     for row in reader:
-        data_type = row[2]
-        data = {
-            "identifier": row[0],
-            "value": row[1],
-            "type": data_type
-        }
-        print("Inserting::" + str(data))
-        if data_type == "Anatomy":
-            insert_data(data, db.anatomy)
-        if data_type == "Gene":
-            insert_data(data, db.gene)
-        if data_type == "Disease":
-            insert_data(data, db.disease)
+        if count > 0:
+            data_type = row[2]
+            data = {
+                "identifier": row[0],
+                "value": row[1],
+                "type": data_type
+            }
+            if data_type == "Anatomy":
+                insert_data(data, db.anatomy)
+            if data_type == "Gene":
+                insert_data(data, db.gene)
+            if data_type == "Disease":
+                insert_data(data, db.disease)
 
-# Loading edges...there are a lot!!!
+        count += 1
+        insert_progress_printer(node_file_total_lines, count, 'Node Insert', 300, True)
+    insert_progress_printer(node_file_total_lines, count, 'Node Insert', 1, True)
+
+
+# Loading edges: 1.3mil count
+edges_file_total_lines = sum(1 for line in open('../data/edges.tsv'))
 with open('../data/edges.tsv') as tsvin:
     reader = csv.reader(tsvin, delimiter='\t')
     count = 0
+    print("\n\nInserting Edges...this could take a while....\n\n")
     for row in reader:
         if count > 0:
             data = {
@@ -33,11 +52,11 @@ with open('../data/edges.tsv') as tsvin:
                 "edge_type": row[1],
                 "target_id": row[2]
             }
-            if count % 10000 == 0:
-                print("Inserting Edge(" + str(count) + "):: " + str(data))
 
             insert_data(data, db.edges)
 
         count += 1
+        insert_progress_printer(edges_file_total_lines, count, 'Edges Insert', 15000, False)
+    insert_progress_printer(node_file_total_lines, count, 'Node Insert', 1, False)
 
 tsvin.close()
