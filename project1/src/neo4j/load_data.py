030a476dec4ede40efe7e2a42a49316b83d19151
helloye <hello.paingy@gmail.com>
2019-10-13 11:44:27 -0400

    adding try catch block to neo4j connection...should probably do the same for mongo_db?

--- project1/src/neo4j/load_data.py
+++ project1/src/neo4j/load_data.py
@@ -1,8 +1,10 @@
 from neo4j import GraphDatabase
 
 uri = 'bolt://localhost:7687'
-driver = GraphDatabase.driver(uri, auth=("neo4j", "neo4j"))
 
-if driver:
-    print('Connection established!!')
-    print(driver)
+try:
+    driver = GraphDatabase.driver(uri, auth=("neo4j", "neo4j"))
+    print('Neo4j Connection Established!!')
+except:
+    print('Neo4j Connection Error: ' + uri)
+
