a8fa8553b140179eeabfadc78439f3a2db7f5b3c
helloye <hello.paingy@gmail.com>
2019-10-13 21:18:40 -0400

    test loading data into neo4j data loader

--- project1/src/neo4j/load_data.py
+++ project1/src/neo4j/load_data.py
@@ -1,3 +1,4 @@
+import csv
 from neo4j import GraphDatabase
 
 uri = 'bolt://localhost:7687'
@@ -8,3 +9,15 @@ try:
 except:
     print('Neo4j Connection Error: ' + uri)
 
+with open('../../data/nodes.tsv') as tsvin:
+    reader = csv.reader(tsvin, delimiter='\t')
+    count = 0
+    for row in reader:
+        # print(row)
+        if count is 0:
+            print(row)
+        count += 1
+
+"""
+Need to extract both node and edge files into memory and then create the graph.
+"""
